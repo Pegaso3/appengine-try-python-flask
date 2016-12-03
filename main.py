ff20ea234332dd978b3ab29eeded111d1cdafa09
Pegaso3 <pabloegas3@gmail.com>
2016-12-03 16:50:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Maestria en Informatica Taller Cloud!'
 
 
 @app.errorhandler(404)
